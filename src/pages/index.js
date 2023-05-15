bace42ab3624efac85fb7b6da573688bc689e79a
Kawengian G <geraldodkawengianj@gmail.com>
2023-05-15 10:46:04 +0800

    adding folder pages

---
+++ src/pages/index.js
@@ -0,0 +1,10 @@
+import SplashScreen from './SplashScreen';
+import SignIn from './SignIn';
+import SignUp from './SignUp';
+import Home from './Home';
+import Profile from './Profile';
+import Menu1 from './Menu1';
+import Menu2 from './Menu2';
+import SellCar from './SellCar';
+
+export {SplashScreen, SignIn, SignUp, Home, Profile, Menu1, Menu2, SellCar};
